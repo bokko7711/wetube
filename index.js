62c6c15e2b46e0c7b8d486490009f0780330265c
geonwoo <78127896+bokko7711@users.noreply.github.com>
2021-03-31 17:43:14 +0900

    diy added

--- index.js
+++ index.js
@@ -20,3 +20,21 @@ app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
+//diy
+/*const express = require('express');
+const app = express();
+const PORT = 4000;
+
+function sendDefaultScreenData(req, res) {
+    res.send("this is DefaultScreen.");
+    console.log("response finished.");
+}
+
+function whenServerRequested() {
+    console.log("now listening...");
+}
+
+app.get("/", sendDefaultScreenData);
+
+app.listen(PORT, whenServerRequested);
+*/
