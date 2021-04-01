274ee1ec405d33daac1299f8ba0b01b3121d0a78
geonwoo <78127896+bokko7711@users.noreply.github.com>
2021-04-01 14:20:48 +0900

    arrowFunction and middleWare

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const express = require('express');
+import express from "express";
 const app = express();
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () => {
     console.log(`listening started on port ${PORT}.`);
 }
 
-function handleHome(req, res) {
-    console.log(req);
-    res.send("hello - from home");
-}
+const handleHome = (req, res) => res.send("hello - from home");
 
-function handleProfile(req, res) {
-    res.send("you are on my profile");
-}
+const handleProfile = (req, res) => res.send("you are on my profile");//arrow function이라고 함.
+
+const middleWareOfHome = (req, res, next) => {
+    console.log("middleWare function operated.");
+    next();
+};
+
+app.use(middleWareOfHome);/** */
+//middleWares as you want...(get의 위쪽에 있어야 middleware로서 작동함.)
 
 app.get("/", handleHome);
+//app.get("/", middleWareOfHome, handleHome);
 
 app.get("/profile", handleProfile);
 
