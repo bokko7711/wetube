632ec4963177983ef2bd9cb6e8d7d46562d2951d
geonwoo <78127896+bokko7711@users.noreply.github.com>
2021-04-04 19:54:51 +0900

    router seperated

---
+++ routers/userRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const userRouter = express.Router();
+
+export default userRouter;
+/**
+ * m - model => data. {database}
+ * v - view => how does the data look {template}
+ * c - control => function that looks for the data (data를 보여주기 위한 함수) {javascript function}
+ */
---
+++ routers/videoRouter.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const videoRouter = express.Router();//앞에 export가 붙으면 이 변수만 export한다는 뜻.
+
+export default videoRouter;//default - file로 export한다는 뜻.
