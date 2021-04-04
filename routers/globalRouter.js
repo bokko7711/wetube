dcc15b4077a585a7d42618d072eee50a85af16b4
geonwoo <78127896+bokko7711@users.noreply.github.com>
2021-04-04 20:30:50 +0900

    routing variables set

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import routes from "../routes";// 온점을 2개 쓴 ../는 상위 폴더에서 찾는다는 의미. ().=>위로, /=>아래로)
 
 const globalRouter = express.Router();
+
+globalRouter.get(routes.home, (req, res) => res.send("home"));
+globalRouter.get(routes.join, (req, res) => res.send("join"));
+globalRouter.get(routes.login, (req, res) => res.send("login"));
+globalRouter.get(routes.logout, (req, res) => res.send("logout"));
+globalRouter.get(routes.search, (req, res) => res.send("search"));
 
 export default globalRouter;
