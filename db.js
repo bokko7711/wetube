c3a14937cacf9600ca3097637c1ce08ca5f9e233
geonwoo <78127896+bokko7711@users.noreply.github.com>
2021-04-11 22:03:30 +0900

    4.1 finished

--- db.js
+++ db.js
@@ -1,50 +1,15 @@
-export const videos = [
-    {
-        id: 111,
-        title: 'Video 111',
-        description: 'it is awesome',
-        views: 9999,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 84,
-            name: "bokko",
-            email: "bokko7711@naver.com"
-        }
-    },
-    {
-        id: 112,
-        title: 'Video 112',
-        description: 'it is awesome2',
-        views: 9539,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 84,
-            name: "bokko",
-            email: "bokko7711@naver.com"
-        }
-    },
-    {
-        id: 113,
-        title: 'Video 113',
-        description: 'it is awesome3',
-        views: 9123,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 84,
-            name: "bokkofdgsg",
-            email: "bokko77@naver.com"
-        }
-    },
-    {
-        id: 114,
-        title: 'Video 114',
-        description: 'it is awesome4',
-        views: 9999,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 84,
-            name: "bokko",
-            email: "bokko771@naver.com"
-        }
-    }
-]
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+    useNewUrlParser: true,//이 부분들은 최신 버전의 mongoose를 사용하면 자동으로 설정되어있는거지만, 그래도 확실하게 하기 위해 써두는거임. 신경ㄴㄴ
+    useFindAndModify: false
+});
+//mongoose connection을 열어 database와 연결될 수 있게 해줌.
+//mongoose.connect("mongodb://localhost:포트번호(mongod를 통해 확인가능)/database이름");
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✔ connected to DB");
+const handleError = (error) => console.log(`❌error on DB connection : ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
--- init.js
+++ init.js
@@ -1,9 +1,10 @@
+import "./db";//이렇게...import할 수도 있었나..?
 import app from "./app";
 
 const PORT = 4000;
 
 const whenServerRequested = () => {
-    console.log("now listening...");
+    console.log("✔ now listening...");
 }
 
 app.listen(PORT, whenServerRequested);
