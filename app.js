6b2fa43aab7f4cb1c5f63b3e042ceca737c5ebc7
geonwoo <78127896+bokko7711@users.noreply.github.com>
2021-04-15 21:16:19 +0900

    6.2 finished - but need to css something

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(function (req, res, next) {//동영상을 볼 수가 없어서 일단 �
 });
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,63 @@
 const path = require("path");
+const autoprefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
-
 const config = {
-    entry: ENTRY_FILE,
+    entry: ["@babel/polyfill", ENTRY_FILE],
+    mode: MODE,
+    devtool: "cheap-module-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins() {
+                                    ㅤㅤㅤㅤreturn[autoprefixer({ browsers: "cover 99.5%" })]
+                                    ㅤㅤㅤ
+                                }
+                            }
+                        }
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            }
+        ]
+    },
     output: {
         path: OUTPUT_DIR,
-        filename: "[name].[format]"
-    }
+        filename: "[name].js"
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: 'styles.css'
+        }),
+    ]
 };
 
 module.exports = config;
